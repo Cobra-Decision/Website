07e72a1f57adad418c421983f4b70b069b4a5ca5
MohsenBg <moh.1380.1393@gmail.com>
2025-12-14 15:46:26 +0330

    feat(app): update AppModule and main bootstrap with global pipes, filters, swagger and seeding

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,12 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
-import { BookmarkModule } from './bookmark/bookmark.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { RoleModule } from './role/role.module';
+import { PermissionModule } from './permission/permission.module';
+import { RouteModule } from './route/route.module';
 
 @Module({
-  imports: [AuthModule, UserModule, BookmarkModule, PrismaModule],
-  controllers: [],
-  providers: [],
+  imports: [
+    AuthModule,
+    UserModule,
+    PrismaModule,
+    RoleModule,
+    PermissionModule,
+    RouteModule,
+  ],
 })
 export class AppModule {}
